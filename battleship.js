2d013ac5d87d8b58c7e3d530e68b48cbea8a698d
Dima <benensondimitrii@gmail.com>
2022-09-17 12:34:10 +0300

    initial commit

---
+++ battleship.js
@@ -0,0 +1,42 @@
+// let location1 = 1;
+// let location2 = 2;
+// let location3 = 3;
+
+// let guess;
+// let hits = 0;
+// let guesses = 0;
+
+// let isSunk = false;
+// console.log("guess!?");
+
+
+
+// while (!isSunk) {
+//   guess = prompt("Ready, aim, fire! (enter a number 0-6):");
+//   console.log("guess");
+//   if (guess > 6 || guess < 0) {
+//     alert("enter valid number");
+//   } else {
+//     guesses += 1;
+//   }
+//   if (location1 == guess || location2 == guess || location3 == guess) {
+//     hits += 1;
+//     console.log(hits);
+//     alert("HIT");
+//     if (hits == 3) {
+//       isSunk = true;
+//       alert("you sank my battleship");
+//     }
+//   } else {
+//     alert("MISS");
+//   }
+  
+// }
+// let stats =
+//   "You took " +
+//   guesses +
+//   " guesses to sink the battleship, " +
+//   "which means your shooting accuracy was " +
+//   3 / guesses;
+
+// alert(stats);
