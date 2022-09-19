835269453d8f24585af5efe550ed92dbe76d2ce9
Dima <benensondimitrii@gmail.com>
2022-09-19 18:05:39 +0300

    added representation object and tested it

--- battleship.js
+++ battleship.js
@@ -1,42 +1,23 @@
-// let location1 = 1;
-// let location2 = 2;
-// let location3 = 3;
+const view = {
+  displayMessage: function (msg) {
+    const messageArea = document.getElementById("messageArea")
+    messageArea.innerHTML = msg
+  },
+  displayHit: function (location){
+    const field = document.getElementById(location)
+    field.setAttribute("class", "hit" )
+  },
+  displayMiss: function (location){
+    const field = document.getElementById(location)
+    field.setAttribute("class", "miss" )
+  },
+};
 
-// let guess;
-// let hits = 0;
-// let guesses = 0;
+view.displayMiss("00")
+view.displayHit("34")
+view.displayMiss("55")
+view.displayHit("12")
+view.displayMiss("25")
+view.displayHit("26")
 
-// let isSunk = false;
-// console.log("guess!?");
-
-
-
-// while (!isSunk) {
-//   guess = prompt("Ready, aim, fire! (enter a number 0-6):");
-//   console.log("guess");
-//   if (guess > 6 || guess < 0) {
-//     alert("enter valid number");
-//   } else {
-//     guesses += 1;
-//   }
-//   if (location1 == guess || location2 == guess || location3 == guess) {
-//     hits += 1;
-//     console.log(hits);
-//     alert("HIT");
-//     if (hits == 3) {
-//       isSunk = true;
-//       alert("you sank my battleship");
-//     }
-//   } else {
-//     alert("MISS");
-//   }
-  
-// }
-// let stats =
-//   "You took " +
-//   guesses +
-//   " guesses to sink the battleship, " +
-//   "which means your shooting accuracy was " +
-//   3 / guesses;
-
-// alert(stats);
+view.displayMessage("bla bla")
