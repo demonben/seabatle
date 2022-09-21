2d5e11aaa38594308932b6aac9b7e3b31d1bb791
Dima <benensondimitrii@gmail.com>
2022-09-21 19:08:28 +0300

    creating object model

--- battleship.js
+++ battleship.js
@@ -1,23 +1,46 @@
 const view = {
   displayMessage: function (msg) {
-    const messageArea = document.getElementById("messageArea")
-    messageArea.innerHTML = msg
+    const messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
   },
-  displayHit: function (location){
-    const field = document.getElementById(location)
-    field.setAttribute("class", "hit" )
+  displayHit: function (location) {
+    const field = document.getElementById(location);
+    field.setAttribute("class", "hit");
   },
-  displayMiss: function (location){
-    const field = document.getElementById(location)
-    field.setAttribute("class", "miss" )
+  displayMiss: function (location) {
+    const field = document.getElementById(location);
+    field.setAttribute("class", "miss");
   },
 };
 
-view.displayMiss("00")
-view.displayHit("34")
-view.displayMiss("55")
-view.displayHit("12")
-view.displayMiss("25")
-view.displayHit("26")
+const model = {
+  boardSize: 7,
+  numShips: 3,
+  shipsSunk: 0,
+  shipLength: 3,
+  ships: [
+    { locations: ["06", "16", "26"], hits: ["", "", ""] },
+    { locations: ["24", "34", "44"], hits: ["", "", ""] },
+    { locations: ["10", "11", "12"], hits: ["", "", ""] },
+  ],
+  fire: function (guess) {
+    for (let i = 0; i < this.ships.length; i++) {
+      const ship = this.ship[i];
+      const index = ship.locations.indexOf(guess);
+      if (index >= 0) {
+        ship.hits[index] = "hit";
+        return true;
+      }
+      return false;
+    }
+  },
+};
 
-view.displayMessage("bla bla")
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+
+view.displayMessage("bla bla");
