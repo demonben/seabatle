7c4ac9ef236fd0ce3cf11035af57e04865c7baa2
Dima <benensondimitrii@gmail.com>
2022-09-22 13:25:18 +0300

    created and tested controller object

--- battleship.js
+++ battleship.js
@@ -52,6 +52,47 @@ const model = {
   },
 };
 
+const controller = {
+  guess: 0,
+  processGuess: function (guess) {
+    let location = parseGuess(guess);
+    if (location) {
+      this.guess++;
+      let hit = model.fire(location);
+      if (hit && model.shipsSunk === model.numShips) {
+        view.displayMessage(
+          "You sank all my battleships, in" + this.guess + "guesses"
+        );
+      }
+    }
+  },
+};
+
+function parseGuess(guess) {
+  const alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+  if (guess === null || guess.length !== 2) {
+    alert("Oops, please enter valid coordinate");
+  } else {
+    let firstChar = guess.charAt(0);
+    let row = alphabet.indexOf(firstChar);
+    let column = guess.charAt(1);
+
+    if (isNaN(row) || isNaN(column)) {
+      alert("Oops, that isn't no the board");
+    } else if (
+      row < 0 ||
+      row > model.boardSize ||
+      column < 0 ||
+      column > model.boardSize
+    ) {
+      alert("Oops, that off the board");
+    } else {
+      return row + column;
+    }
+  }
+  return null;
+}
+//test
 // view.displayMiss("00");
 // view.displayHit("34");
 // view.displayMiss("55");
@@ -61,12 +102,29 @@ const model = {
 
 // view.displayMessage("bla bla");
 
-model.fire("12");
-model.fire("11");
-model.fire("10");
+// model.fire("12");
+// model.fire("11");
+// model.fire("10");
 
-model.fire("06");
-model.fire("16");
-model.fire("26");
+// model.fire("06");
+// model.fire("16");
+// model.fire("26");
 
-model.fire("53");
+// model.fire("53");
+
+// console.log(parseGuess("A0"));
+// console.log(parseGuess("B6"));
+// console.log(parseGuess("G3"));
+// console.log(parseGuess("H0"));
+// console.log(parseGuess("A7"));
+
+// controller.processGuess("A0");
+// controller.processGuess("A6");
+// controller.processGuess("B6");
+// controller.processGuess("C6");
+// controller.processGuess("C4");
+// controller.processGuess("D4");
+// controller.processGuess("E4");
+// controller.processGuess("B0");
+// controller.processGuess("B1");
+// controller.processGuess("B2");
